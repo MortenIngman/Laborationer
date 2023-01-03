3583d31c19fafc5a8967100edc40dbcd10f01763
Ingman-dev <morten.ingman@ingmandev.se>
2023-01-03 14:59:27 +0100

    lab 3 klar

--- src/laboration1/laboration3/HeltalsHanterare.java
+++ src/laboration1/laboration3/HeltalsHanterare.java
@@ -5,12 +5,30 @@ import java.util.Scanner;
 
 public class HeltalsHanterare {
     public static void main(String[] args) {
-
         Scanner input = new Scanner(System.in);
-        int antalHeltal;
-        ArrayList<Integer> heltalsArray = new ArrayList<>();
-        int summa = 0;
 
+        int antalHeltal;
+        int medelVarde;
+        int frekvens;
+
+        ArrayList<Integer> heltalsArray = new ArrayList<>();
+        ArrayList<Integer> dublettArray = new ArrayList<>();
+        ArrayList<Integer> singelArray = new ArrayList<>();
+
+        antalHeltal = antalHeltal(input);
+        skapaHeltalsArray(input, antalHeltal, heltalsArray);
+        frekvens = beraknaForekomster(heltalsArray, dublettArray, singelArray);
+        medelVarde = beraknaMedelvarde(heltalsArray);
+
+        System.out.println("Du angav " + antalHeltal + " tal.");
+        System.out.println("Varav " + frekvens + " bara angavs en gång.");
+        System.out.println("Medelvärdet för talen är: " + medelVarde);
+
+        input.close();
+    }
+
+    private static int antalHeltal(Scanner input) {
+        int antalHeltal;
         while(true) {
             System.out.println("Hur många heltal vill du ange? ");
             String heltal = input.nextLine();
@@ -22,29 +40,50 @@ public class HeltalsHanterare {
                 System.out.println("Ogiltigt värde!");
             }
         }
+        return antalHeltal;
+    }
 
+    private static void skapaHeltalsArray(Scanner input, int antalHeltal, ArrayList<Integer> heltalsArray) {
         for(int i = 1; i <= antalHeltal; i++) {
-                while(true) {
-                    System.out.println("Ange heltal " + i);
-                    String heltal = input.nextLine();
-                    try {
-                        heltalsArray.add(Integer.parseInt(heltal));
+            while(true) {
+                System.out.println("Ange heltal " + i);
+                String heltal = input.nextLine();
+                try {
+                    heltalsArray.add(Integer.parseInt(heltal));
+                    break;
+                } catch (NumberFormatException e) {
+                    System.out.println("Ogiltigt värde!");
+                }
+            }
+        }
+    }
+
+    private static int beraknaForekomster(ArrayList<Integer> heltalsArray, ArrayList<Integer> dublettArray, ArrayList<Integer> singelArray) {
+        boolean arDublett;
+        for (int i = 0; i < heltalsArray.size(); i++) {
+            arDublett = false;
+            if (!dublettArray.contains(heltalsArray.get(i))){
+                for (int j = i + 1; j < heltalsArray.size(); j++) {
+                    if (heltalsArray.get(i).equals(heltalsArray.get(j))) {
+                        arDublett = true;
                         break;
-                    } catch (NumberFormatException e) {
-                        System.out.println("Ogiltigt värde!");
                     }
                 }
+                if (!arDublett) {
+                    singelArray.add(heltalsArray.get(i));
+                } else {
+                    dublettArray.add(heltalsArray.get(i));
+                }
+            }
         }
+        return singelArray.size();
+    }
 
-        System.out.println(heltalsArray);
-
+    private static int beraknaMedelvarde(ArrayList<Integer> heltalsArray) {
+        int summa = 0;
         for (Integer integer : heltalsArray) {
             summa += integer;
         }
-
-        System.out.println(summa);
-
+        return summa / heltalsArray.size();
     }
-
-
 }
